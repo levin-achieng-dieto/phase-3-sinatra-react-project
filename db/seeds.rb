59fce844d756e1fe5567ca3e905b6a3a8c1e0ae4
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-11-09 14:17:11 +0300

    done seeding databases

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,26 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+25.times do
+    Post.create(
+        title: Faker::Movie.unique.title,
+        image: "https://loremflickr.com/#{rand(150..200)}/#{rand(150..200)}/all",
+        short_story: Faker::Lorem.unique.paragraph(sentence_count: 20)
+    )
+end
+
+50.times do
+    User.create(
+        name: Faker::Name.unique.name
+    )
+end
+
+50.times do
+    Like.create(
+        user_id: User.all[rand(User.count)].id,
+        post_id: Post.all[rand(Post.count)].id
+    )
+end
 
 
 puts "✅ Done seeding!"
