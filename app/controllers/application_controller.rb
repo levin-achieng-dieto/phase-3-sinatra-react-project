32d3bbb75e8a6ee362bd1f7c2e6005a6bfeb6407
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-11-09 15:31:43 +0300

    doing crud operations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,37 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "this file Good luck with your project!" }.to_json
+    { message: "Good luck with your project!" }.to_json
   end
 
-  
+  get '/posts' do
+    posts = Post.all
+    posts.to_json(inlude: :likes)
+  end
+   
+  post '/posts' do
+    post = Post.create(
+      title: params[:title],
+      image: params[:image],
+      short_story: params[:short_story]
+    )
+    post.to_json
+  end
+
+  patch '/posts/:id' do
+    post = Post.find(params[:id])
+    post.update(
+      title: params[:title],
+      image: params[:image],
+      short_story: params[:short_story]
+    )
+    post.to_json
+  end
+
+  delete '/posts/:id' do
+    post = Post.find(params[:id])
+    post.destroy
+    post.to_json
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
-25.times do
+10.times do
     Post.create(
         title: Faker::Movie.unique.title,
-        image: "https://loremflickr.com/#{rand(150..200)}/#{rand(150..200)}/all",
+        image: "https://loremflickr.com/#{rand(100..200)}/#{rand(150..200)}/all",
         short_story: Faker::Lorem.unique.paragraph(sentence_count: 20)
     )
 end
 
-50.times do
+10.times do
     User.create(
         name: Faker::Name.unique.name
     )
 end
 
-50.times do
+100.times do
     Like.create(
         user_id: User.all[rand(User.count)].id,
         post_id: Post.all[rand(Post.count)].id
