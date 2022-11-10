e18561a2ea5d54bc88fdaad18291a9d0e4f69892
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-11-10 07:48:20 +0300

    seeding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "this time Good luck with your project!" }.to_json
   end
 
   get '/posts' do
@@ -11,29 +11,29 @@ class ApplicationController < Sinatra::Base
     posts.to_json(inlude: :likes)
   end
    
-  post '/posts' do
-    post = Post.create(
-      title: params[:title],
-      image: params[:image],
-      short_story: params[:short_story]
-    )
-    post.to_json
-  end
+  # post '/posts' do
+  #   post = Post.create(
+  #     title: params[:title],
+  #     image: params[:image],
+  #     short_story: params[:short_story]
+  #   )
+  #   post.to_json
+  # end
 
-  patch '/posts/:id' do
-    post = Post.find(params[:id])
-    post.update(
-      title: params[:title],
-      image: params[:image],
-      short_story: params[:short_story]
-    )
-    post.to_json
-  end
+  # patch '/posts/:id' do
+  #   post = Post.find(params[:id])
+  #   post.update(
+  #     title: params[:title],
+  #     image: params[:image],
+  #     short_story: params[:short_story]
+  #   )
+  #   post.to_json
+  # end
 
-  delete '/posts/:id' do
-    post = Post.find(params[:id])
-    post.destroy
-    post.to_json
-  end
+  # delete '/posts/:id' do
+  #   post = Post.find(params[:id])
+  #   post.destroy
+  #   post.to_json
+  # end
 
 end
